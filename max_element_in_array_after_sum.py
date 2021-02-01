01e62c4f4252dbe858a529a933f82c6e1180389f
Hitesh Ingale <dev.hitesh97@gmail.com>
2021-02-01 14:09:42 +0530

    Create max_element_in_array_after_sum.py
    Given the sample question:
    
    a b k
    1 5 3
    4 8 7
    6 9 1
    
    The resulting transformations to the array are
    ```
     1  2  3  4  5   6  7  8   9  10
    [0, 0, 0, 0, 0,  0, 0, 0,  0, 0]
    [3, 0, 0, 0, 0, -3, 0, 0,  0, 0] // a: 1, b: 5, k: 3, so b+1 = 6
    [3, 0, 0, 7, 0, -3, 0, 0, -7, 0]// a: 4: , b: 8, k: 7, so b+1 = 9
    [3, 0, 0, 7, 0, -2, 0, 0, -7, -1] // a: 6, b: 9, k: 1, so b+1 = 10
    ```
    
    you iterate through the final state of the array, noting when the slope increase and comparing that value to the max value so far until you reach the end. In your example above, it you start at 3, then you see a 7, which adds to 10. All subsequent slopes are negative and so the max is 10.

---
+++ max_element_in_array_after_sum.py
@@ -0,0 +1,32 @@
+# find the max element in an array of size N after K number of additions
+#ref: https://www.hackerrank.com/challenges/crush/problem
+
+def arrayManipulation(n, queries):
+    intial = []
+    for _ in range(1, n+1):
+        intial.append(0)
+    for query in queries:
+        for i in range(query[0]-1, query[1]):
+            intial[i] += query[2]  
+    return max(intial)
+    
+def optimised(n, queries):
+    intial = []
+    _max = 0
+    x = 0
+    
+    for _ in range(1, n+1):
+        intial.append(0)
+        
+    for query in queries:
+        intial[query[0]] += query[2]
+        if query[1]+1 <= len(intial) - 1: intial[query[1]+1] -= query[2]
+    
+    for i in intial:
+        x += i
+        _max = max(x, _max)
+    return _max
+    
+    
+# Driver code:
+print(arrayManipulation(10, [[1,5,3], [4,8,7], [6,9,1]]))
