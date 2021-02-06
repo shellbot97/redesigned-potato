8f9a2efcdc1ce26a9deb4aeeff72b96a04f11aff
Hitesh Ingale <dev.hitesh97@gmail.com>
2021-02-06 16:42:43 +0530

    Update max_number_after_adding_5.py

--- tests/max_number_after_adding_5.py
+++ tests/max_number_after_adding_5.py
@@ -14,4 +14,19 @@ def solution(N):
         new_number = int(string_number[:i] + '5' + string_number[i:])
         result = max(new_number, int(result))
     return result
+
+def solution2(N):
+    string_number = str(N)
+    
+    if string_number[0] == "-":
+        result = "-5"+string_number.replace("-", "")
+    else:
+        result = 0
+        
+    for i in range(len(string_number)):
+        if(string_number[i] != "-"):
+            new_number = int(string_number[:i] + '5' + string_number[i:])
+            result = max(new_number, int(result))
+    return result
+
 print(solution(-999))
