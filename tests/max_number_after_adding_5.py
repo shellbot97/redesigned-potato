a99473e587e387cbbbc1388b9e0e2710869ee11a
Hitesh Ingale <dev.hitesh97@gmail.com>
2021-02-06 11:03:04 +0530

    Create max_number_after_adding_5.py

---
+++ tests/max_number_after_adding_5.py
@@ -0,0 +1,17 @@
+# ref: https://www.tutorialspoint.com/insert-5-to-make-number-largest-in-python
+
+def solution(N):
+    string_number = str(N)
+    
+    if string_number[0] == "-":
+        result = "-"+"5"+string_number.replace("-", "")
+    else:
+        result = 0
+        
+    for i in range(len(string_number) + 1):
+        if(string_number[0] == "-"):
+            continue
+        new_number = int(string_number[:i] + '5' + string_number[i:])
+        result = max(new_number, int(result))
+    return result
+print(solution(-999))
