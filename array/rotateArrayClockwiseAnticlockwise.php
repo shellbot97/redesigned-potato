321ea82cd18947df01c08d07de72d08d1e81d089
Hitesh Ingale <dev.hitesh97@gmail.com>
2022-01-10 20:57:15 +0530

    Create rotateArrayClockwiseAnticlockwise.php

---
+++ array/rotateArrayClockwiseAnticlockwise.php
@@ -0,0 +1,24 @@
+<?php
+// Rotate array clockwise/anticlockwise
+// https://paiza.io/projects/PLyCJ2a3Kr7SzG-pLbPQfw?language=php
+
+$testCase1 = [1,2,3,4,5,6,7,8,9,0];
+$step = 2;
+
+function rotateArrayCyclically(string $direction = 'a', array $arrayToBeRotated = []) : array
+{
+    if(count($arrayToBeRotated) == 0){
+        return $arrayToBeRotated;
+    }
+    
+}
+
+
+function swapElementsAtIndices(array $arrayToBeSwapped = [], int $index = 0, int $index2 = 0)
+{
+    if (($index1 == $index2) || ($index1 == 0) || ($index2 == 0) || !isset($arrayToBeRotated[$index1]) || !isset($arrayToBeRotated[$index2])) {
+        return $arrayToBeSwapped;
+    }
+}
+
+?>
