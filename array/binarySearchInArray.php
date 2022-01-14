679bb18070b40d4eb23ae1b9e815b6745fb17880
Hitesh Ingale <dev.hitesh97@gmail.com>
2022-01-14 16:39:34 +0530

    Create binarySearchInArray.php

---
+++ array/binarySearchInArray.php
@@ -0,0 +1,36 @@
+<?php
+// https://www.geeksforgeeks.org/binary-search/
+// https://paiza.io/projects/RktPPPv_pq9RrvDp1joRmg?language=php
+
+$testCase1 = [1,2,3,4,5,6,7,8,9];
+
+function binarySearchInArray($arrayToBeSearched = [], $valueToBeSearched = '') : int
+{
+    $sizeOfArray = count($arrayToBeSearched);
+    if ($sizeOfArray == 0 || $valueToBeSearched == '') {
+        return -1;
+    }
+    $middleIndex = floor($sizeOfArray/2);
+    if($arrayToBeSearched[$middleIndex] == $valueToBeSearched){
+        return $middleIndex;
+    }
+    if ($arrayToBeSearched[$middleIndex] > $valueToBeSearched) {
+        for ($i = 0; $i < $middleIndex; $i++) {
+            if($arrayToBeSearched[$i] == $valueToBeSearched){
+                return $i;
+            }
+        }
+    }else{
+        for ($i = $middleIndex+1; $i < $sizeOfArray; $i++) {
+            if($arrayToBeSearched[$i] == $valueToBeSearched){
+                return $i;
+            }
+        }
+    }
+    return -2;
+}
+
+
+print_r(binarySearchInArray($testCase1, 8));
+
+?>
