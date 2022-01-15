c86527b69408255bf889c94ff92d15d4a89c7105
Hitesh Ingale <dev.hitesh97@gmail.com>
2022-01-15 20:11:08 +0530

    Create SearchElementInRotatedSortedArray.php

---
+++ array/SearchElementInRotatedSortedArray.php
@@ -0,0 +1,24 @@
+<?php
+// https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
+// https://paiza.io/projects/TeWNffWBC8iEycDsLxnkLg?language=php
+
+$testCase1 = [4,5,6,7,8,9,1,2,3];
+$findElement = 7;
+
+/**
+ * 
+**/
+function searchArrayInRotatedSortedArray(array $arrayToBeTraversed = [], int $elementToBeFound = 0) : int
+{
+    
+}
+
+function findPivotalPoint() : int
+{
+    
+}
+
+// Driver code
+print_r(searchArrayInRotatedSortedArray($testCase1, $findElement));
+
+?>
