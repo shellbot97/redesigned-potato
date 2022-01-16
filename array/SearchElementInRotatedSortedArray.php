754ef10e64cea9f7af8adf62bf4db25dec0c7499
Hitesh Ingale <dev.hitesh97@gmail.com>
2022-01-16 10:09:34 +0530

    Update SearchElementInRotatedSortedArray.php

--- array/SearchElementInRotatedSortedArray.php
+++ array/SearchElementInRotatedSortedArray.php
@@ -3,22 +3,97 @@
 // https://paiza.io/projects/TeWNffWBC8iEycDsLxnkLg?language=php
 
 $testCase1 = [4,5,6,7,8,9,1,2,3];
-$findElement = 7;
+$findElement = 1;
 
 /**
- * 
+ * find pivotal point
+ * if valid pivotal point
+ *      if pivotalPoint > $elementToBeFound > firstIndex
+ *          binarySearch(firstIndex to pivotal point)
+ *      elseif pivotalPoint+1 > elementToBeFound > lastIndex
+ *          binarySearch(pivotalPoint+1 to lastIndex)
+ * else
+ *      binarySearch(firstIndex to lastIndex)
 **/
 function searchArrayInRotatedSortedArray(array $arrayToBeTraversed = [], int $elementToBeFound = 0) : int
 {
-    
+    $pivotalPoint = findPivotalPoint($arrayToBeTraversed);
+    if($pivotalPoint > -1){
+        if (($arrayToBeTraversed[$pivotalPoint] > $elementToBeFound) && ($elementToBeFound > $arrayToBeTraversed[0])) {
+            $slicedArray = array_slice($arrayToBeTraversed, 0, $pivotalPoint);
+            return binarySearchInArray($slicedArray, $elementToBeFound);
+        }elseif(end($arrayToBeTraversed) > $elementToBeFound) && ($elementToBeFound > $arrayToBeTraversed[$pivotalPoint+1])){
+            $slicedArray = array_slice($arrayToBeTraversed, $pivotalPoint+1, count($arrayToBeTraversed));
+            return binarySearchInArray($slicedArray, $elementToBeFound);
+        }
+    }else{
+        return binarySearchInArray($arrayToBeTraversed, $elementToBeFound);
+    }
 }
 
-function findPivotalPoint() : int
+/**
+ * finds the pivotal point
+ * 345678123
+ * i+1 has to be bigger then i
+ * get i where i+1 < i
+ * if not found return -1
+ **/
+function findPivotalPoint(array $pivotedArray = []) : int
 {
-    
+    for ($i = 0; $i < count($pivotedArray); $i++) {
+        if ($pivotedArray[$i+1] < $pivotedArray[$i]) {
+            return $i;
+        }
+    }
+    return -1;
+}
+
+
+/**
+ * return -1 of validation fails
+ * -2 if element doesnt exist in array
+ * 
+ * validate
+ *  of sizeOfArray is 0 or valueToBeSearched is empty
+ *  return invalid
+ * if first element is in array is equal to valueToBeSearched
+ *  return 0
+ * get the middleIndex of current arrayToBeSearched
+ * if valueToBeSearched is greater than middle index of arrayToBeSearched
+ *  slice arrayToBeSearched from 0 to middleIndex
+ * elseif valueToBeSearched is between middle index and last index
+ *  slice arrayToBeSearched from middle index and last index
+ * else
+ *  return not found
+**/
+function binarySearchInArray(array $arrayToBeSearched = [], $valueToBeSearched = '') : int
+{
+    $sizeOfArray = count($arrayToBeSearched);
+    if ($sizeOfArray == 0 || $valueToBeSearched == '') {
+        return -1;
+    }
+    if ($arrayToBeSearched[0] == $valueToBeSearched) {
+        return 0;
+    }
+    $middleIndex = floor($sizeOfArray/2);
+    echo $middleIndex."----\n";
+    if($arrayToBeSearched[$middleIndex] == $valueToBeSearched){
+        // echo "=\n";
+        return $middleIndex;
+    }elseif ($arrayToBeSearched[$middleIndex] > $valueToBeSearched) {
+        $recursedIndex = binarySearchInArray(array_slice($arrayToBeSearched, 0, $middleIndex-1), $valueToBeSearched);
+        // echo "$arrayToBeSearched[$middleIndex] > $valueToBeSearched\n";
+        return $recursedIndex;
+    }elseif($arrayToBeSearched[$middleIndex] < $valueToBeSearched && $valueToBeSearched < end($arrayToBeSearched)){
+        $recursedIndex = binarySearchInArray(array_slice($arrayToBeSearched, $middleIndex+1, count($arrayToBeSearched)-$middleIndex), $valueToBeSearched);
+        // echo "$arrayToBeSearched[$middleIndex] < $valueToBeSearched\n";
+        return $middleIndex + 1 + $recursedIndex;
+    }else{
+        return -2;
+    }
 }
 
 // Driver code
 print_r(searchArrayInRotatedSortedArray($testCase1, $findElement));
-
+// print_r(findPivotalPoint($testCase1));
 ?>
