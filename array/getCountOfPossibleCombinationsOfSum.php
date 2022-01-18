7a5d5339978568e3f896457f09abb571ad07b3fb
Hitesh Ingale <dev.hitesh97@gmail.com>
2022-01-18 12:08:26 +0530

    Update getCountOfPossibleCombinationsOfSum.php

--- array/getCountOfPossibleCombinationsOfSum.php
+++ array/getCountOfPossibleCombinationsOfSum.php
@@ -1,27 +1,41 @@
 <?php
 
+// https://paiza.io/projects/glCuwn71K3KTpJBkkDLh7g?language=php
+// brute force solution
+
+
 class Abc
 {
     public function __construct()
     {
-        $this->numbers = [1,2,3,4,5,6,7,8];
-        $this->sum = 10;
-        $this->getCountOfPossibleCombinationsForSum();
+        $this->numbers = [1,2,4,5,6,7,8];
+        $this->sum = 8;
+        $this->combinations = [];
+        $this->getCountOfPossibleCombinationsForSum();    
+        
     }
     
     function getCountOfPossibleCombinationsForSum(){
         if (array_sum($this->numbers) < $this->sum) {
             return -1;
         }
-        $combinations[] = $this->getCombinationsForSum($this->sum);
-        print_r($combinations);
+        for ($i = 0; $i < count($this->numbers); $i++) {
+            $combina = $this->getCombinationsForSum($this->sum);
+            $explodedCombi = explode("|", $combina);
+            sort($explodedCombi);
+            $this->combinations[] = implode("|", $explodedCombi);
+        }
+        print_r($this->combinations);
     }
     
     function getCombinationsForSum($expectedSum){
         $numberExists = array_search($expectedSum, $this->numbers);
         if ($numberExists !== false) {
             echo "0\n";
-            return [$this->numbers[$numberExists]];
+            $combin = implode("|", [$this->numbers[$numberExists]]);
+            if($this->combinationNotExists($combin)){
+                return $combin;
+            }
         }
         $firstDelta = abs(current($this->numbers) - $expectedSum);
         $lastDelta = abs(end($this->numbers) - $expectedSum);
@@ -29,45 +43,70 @@ class Abc
             echo "1\n";
             for ($i = count($this->numbers)-1; $i >= 0; $i--) {
                 $this->sumDelta = $expectedSum - ($this->numbers[$i] + $this->numbers[$i-1]);
-                if ($this->sumDelta == 0) {
+                if ($this->sumDelta == 0 && $this->combinationNotExists()) {
                     echo "1.1\n";
-                    return [$this->numbers[$i], $this->numbers[$i-1]];
+                    $combin = implode("|", [$this->numbers[$i], $this->numbers[$i-1]]);
+                    if($this->combinationNotExists($combin)){
+                        return $combin;
+                    }
                 }
                 elseif($this->sumDelta > 0) {
                     echo "1.2\n";
-                    return array_merge($this->getCombinationsForSum($this->sumDelta), [$this->numbers[$i], $this->numbers[$i-1]]);
+                    $combin = implode("|", array_merge([$this->getCombinationsForSum($this->sumDelta)], [$this->numbers[$i], $this->numbers[$i-1]]));
+                    if($this->combinationNotExists($combin)){
+                        return $combin;
+                    }
                 }
                 elseif($this->sumDelta < 0) {
                     echo "1.3\n";
                     if ($expectedSum > $this->numbers[$i]) {
                         echo "1.3.1\n";
                         $remainingSumDelta = $expectedSum - $this->numbers[$i];
-                        return array_merge($this->getCombinationsForSum($remainingSumDelta), [$this->numbers[$i]]);
+                        $combin = implode("|", array_merge([$this->getCombinationsForSum($remainingSumDelta)], [$this->numbers[$i]]));
+                        if($this->combinationNotExists($combin)){
+                            return $combin;
+                        }
                     }
                 }
             }
         } else {
             echo "2\n";
-            for ($i = 0; $i < count($this->numbers); $i++) {
+            for ($i = 0; $i < count($this->numbers)-1; $i++) {
                 $this->sumDelta = $expectedSum - ($this->numbers[$i] + $this->numbers[$i+1]);
                 if ($this->sumDelta == 0) {
                     echo "2.1\n";
-                    return [$this->numbers[$i], $this->numbers[$i+1]];
+                    $combin = implode("|", [$this->numbers[$i], $this->numbers[$i+1]]);
+                    if($this->combinationNotExists($combin)){
+                        return $combin;
+                    }
                 }
                 elseif($this->sumDelta > 0) {
                     echo "2.2\n";
-                    return array_merge($this->getCombinationsForSum($this->sumDelta), [$this->numbers[$i], $this->numbers[$i+1]]);
+                    $combin = implode("|", array_merge([$this->getCombinationsForSum($this->sumDelta)], [$this->numbers[$i], $this->numbers[$i+1]]));
+                    if($this->combinationNotExists($combin)){
+                        return $combin;
+                    }
                 }
                 elseif($this->sumDelta < 0) {
                     echo "2.3\n";
                     if ($expectedSum > $this->numbers[$i]) {
                         echo "2.3.1\n";
                         $remainingSumDelta = $expectedSum - $this->numbers[$i];
-                        return array_merge($this->getCombinationsForSum($remainingSumDelta), [$this->numbers[$i]]);
+                        $combin = implode("|", array_merge([$this->getCombinationsForSum($remainingSumDelta)], [$this->numbers[$i]]));
+                        if($this->combinationNotExists($combin)){
+                            return $combin;
+                        }
                     }
                 }
             }
         }
+        echo "---\n";
+    }
+    
+    function combinationNotExists($combination){
+        $explodedCombi = explode("|", $combination);
+        sort($explodedCombi);
+        return !in_array(implode("|", $explodedCombi), $this->combinations);
     }
 }
 
